b9b000f223f781d6b11813f3bef47c372b707d80
FelipeFVdev <feliep8@gmail.com>
2024-04-23 23:58:50 -0300

    🎉 Adot-org
    First Commit Rebuild TCC Adto.org 2022

---
+++ nuxt.config.ts
@@ -0,0 +1,45 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  modules: [
+    "@nuxtjs/tailwindcss",
+    "@nuxtjs/color-mode",
+    "@vueuse/nuxt",
+    "nuxt-icon",
+  ],
+
+  css: ["~/assets/css/tailwind.css"],
+
+  tailwindcss: {
+    exposeConfig: true,
+  },
+
+  colorMode: {
+    classSuffix: "",
+  },
+
+  typescript: {
+    shim: false,
+  },
+
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+
+  imports: {
+    imports: [
+      {
+        from: "tailwind-variants",
+        name: "tv",
+      },
+      {
+        from: "tailwind-variants",
+        name: "VariantProps",
+        type: true,
+      },
+    ],
+  },
+});
